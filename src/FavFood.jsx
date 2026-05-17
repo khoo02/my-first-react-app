ff1c7d20e2789a5d11cb5e4773fdd04d29c955cc
Iverson Khoo <khooi1@unlv.nevada.edu>
2026-05-17 03:31:36 -0700

    create FavFood component and display favorite food

---
+++ src/FavFood.jsx
@@ -0,0 +1,10 @@
+function FavFood() {
+  return (
+    <div>
+      <img src="/Pizza_Margherita.jpg" alt="Margherita Pizza" width="300" />
+      <p>My favorite food is margherita pizza.</p>
+    </div>
+  )
+}
+
+export default FavFood;
